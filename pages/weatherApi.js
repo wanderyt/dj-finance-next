e2e60b29629f2be638e12adf0af045aa566d1bec
Ren, Yutao <yuren@ebay.com>
2019-07-16 09:57:18 +0800

    Add css for weather page.

--- pages/weatherApi.js
+++ pages/weatherApi.js
@@ -19,10 +19,25 @@ const Weather = () => {
       {
         weather.code ?
         <div className='Weather'>
-          <div className='City'>上海</div>
+          <div
+            className='City'
+            style={{
+              margin: '10px auto',
+              padding: '10px'
+            }}>上海</div>
           {
             weather.data.forecast.map((item, index) => (
-              <div className='Weather__ForecastItem' key={index}>
+              <div
+                className='Weather__ForecastItem'
+                key={index}
+                style={{
+                  maxWidth: '300px',
+                  margin: '10px',
+                  padding: '10px',
+                  boxShadow: '0px 1px 3px 0px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 2px 1px -1px rgba(0,0,0,0.12)',
+                  borderRadius: '4px',
+                  backgroundColor: '#FFF'
+                }}>
                 <div className='Date'>{item.date}</div>
                 <div className='Temporature'>{`${item.high} - ${item.low}`}</div>
                 <div className='Type'>{item.type}</div>
